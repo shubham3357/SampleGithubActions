6bc28c0c1923eaeb33f0527ae5b3dd05e259a3c3
shubham3357 <shubham3357@outlook.com>
2022-12-05 16:15:02 +0530

    Changed webDriver to BaseClass.webDriver

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -7,17 +7,17 @@ import utilities.BaseClass;
 public class LoginPage extends BaseClass {
 
 	public void setUserName(String uname) {
-		WebElement userName = webDriver.findElement(By.xpath("//input[@name='uid']"));
+		WebElement userName = BaseClass.webDriver.findElement(By.xpath("//input[@name='uid']"));
 		userName.sendKeys(uname);
 	}
 
 	public void setPassword(String pwd) {
-		WebElement password = webDriver.findElement(By.xpath("//input[@name='password']"));
+		WebElement password = BaseClass.webDriver.findElement(By.xpath("//input[@name='password']"));
 		password.sendKeys(pwd);
 	}
 
 	public void clickOnLogin() {
-		WebElement btnLogin = webDriver.findElement(By.xpath("//input[@name='btnLogin']"));
+		WebElement btnLogin = BaseClass.webDriver.findElement(By.xpath("//input[@name='btnLogin']"));
 		btnLogin.click();
 	}
 }
